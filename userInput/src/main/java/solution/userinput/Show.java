93071bb00d3dc048d1936244e4039c89a0d6d011
fahadhasan <fh223430@gmail.com>
2023-09-23 13:49:05 +0600

    Another project added

---
+++ userInput/src/main/java/solution/userinput/Show.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package solution.userinput;
+
+/**
+ *
+ * @author fh223
+ */
+public class Show {
+    public static void main(String[] args) {
+        System.out.println("Hello World form input user!");
+    }
+}
---
+++ userInput/src/main/java/solution/userinput/UserInput.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package solution.userinput;
+
+/**
+ *
+ * @author fh223
+ */
+public class UserInput {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World form input user!");
+    }
+}

cada22003912ce6232997b81fb6509d7cc7128fa
fahadhasan <fh223430@gmail.com>
2023-09-30 23:52:05 +0600

    While Loop

---
+++ userInput/src/main/java/solution/userinput/WhileLoop.java
@@ -0,0 +1,15 @@
+
+package solution.userinput;
+
+
+public class WhileLoop {
+    public static void main(String[]args){
+        //While loop Intialization
+        int i = 1;
+        while (i <= 10){
+            System.out.println(i+ " Bangladesh");
+            i++;
+        }
+    }
+    
+}

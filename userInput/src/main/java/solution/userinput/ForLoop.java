de39bd160583d909b92f9b9ed9cdb0729807d91a
fahadhasan <fh223430@gmail.com>
2023-09-30 21:44:32 +0600

    For Loop

---
+++ userInput/src/main/java/solution/userinput/ForLoop.java
@@ -0,0 +1,15 @@
+
+package solution.userinput;
+import java.util.Scanner;
+public class ForLoop {
+    public static void main (String[]args)
+    {
+       Scanner input = new Scanner(System.in); 
+       //intialization,Condition,increment/decrement
+       for (int i = 2 ; i <= 100; i = i+2){
+           //System.out.println("Hello EveryOne..");
+           System.out.println(i);
+       }
+    }
+           
+}

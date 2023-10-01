c5076ea7551dad43c8eec1a145085613a3794a8a
fahadhasan <fh223430@gmail.com>
2023-10-02 01:32:35 +0600

    Assignment 10

--- userInput/src/main/java/solution/userinput/Assignment10.java
+++ userInput/src/main/java/solution/userinput/Assignment10.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Assignment10 {
     public static void main(String[]args){
+        //user input
         Scanner input = new Scanner (System.in);
         
         for (int i = 1 ; i <= 10 ; i++){

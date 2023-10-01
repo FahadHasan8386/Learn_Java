20447e7e9be36289721c44e2cba3e87675e9cddd
fahadhasan <fh223430@gmail.com>
2023-10-01 21:12:08 +0600

    Assignment 10

---
+++ userInput/src/main/java/solution/userinput/Assignment10.java
@@ -0,0 +1,12 @@
+package solution.userinput;
+import java.util.Scanner;
+
+public class Assignment10 {
+    public static void main(String[]args){
+        Scanner input = new Scanner (System.in);
+        
+        for (int i = 1 ; i <= 10 ; i++){
+            System.out.println("Sum of the number : "+i);
+        }
+    }
+}

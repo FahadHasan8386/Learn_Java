e9b48efaa6225350ee68fe8baa1107f6e4c1a01a
fahadhasan <fh223430@gmail.com>
2023-10-02 22:58:50 +0600

    Assignment 12

---
+++ userInput/src/main/java/solution/userinput/Assignment12.java
@@ -0,0 +1,20 @@
+
+package solution.userinput;
+
+import java.util.Scanner;
+public class Assignment12 {
+    public static void main (String[]args){
+        Scanner input =new Scanner(System.in);
+        System.out.print("Enter first Number : ");
+        int first = input.nextInt();
+        System.out.print("Enter Scoend Number : ");
+        int scoend = input.nextInt();
+        System.out.print("Odd number betwwen first to scoend : ");
+        for (int i = first ; i <= scoend ; i++){
+            if (i % 2 !=0 ){
+                System.out .print(i+ " ");
+            }
+        }
+    }
+    
+}

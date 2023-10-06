8a22f6e23036404e0ffa58cbd36d988a71a7e55d
fahadhasan <fh223430@gmail.com>
2023-10-07 02:09:43 +0600

    Celcius

---
+++ userInput/src/main/java/solution/userinput/Celcius.java
@@ -0,0 +1,10 @@
+
+package solution.userinput;
+
+public class Celcius {
+     public static void main(String[]args){
+     int f=212 , c;
+        c = ((f - 32)*5)/9;
+        System.out.println("Celsius is : "+c);
+    
+}}

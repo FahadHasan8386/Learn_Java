dfd53ee020a036ed24e3e346de8d6d2dddab1f30
fahadhasan <hasan.fahad67@outlook.com>
2023-10-25 19:28:21 +0600

    MyString

--- MyString/MyString.java
+++ MyString/MyString.java
@@ -1,19 +1,28 @@
 import java.lang.*;
 import java.io.*;
+
 public class MyString {
     public static void main(String[] args) {
         String myString = "Hello how are you";
-        
-        
+
         int charCount = myString.length();
         System.out.println("The number of characters in the string is: " + charCount);
-        int count = 0 ;
-		for (int i = 0; i < charCount; i++){
-			if (myString.charAt(i) == ' '){
-				count++;
-			}
-		}
-		System.out.println(count);
-		
-	}
+        int count = 0;
+        for (int i = 0; i < charCount; i++) {
+            if (myString.charAt(i) == ' ') {
+                count++;
+            }
+        }
+        System.out.println("Number of spaces in the string: " + count);
+
+        // Reverse the string and print it
+        String reversedString = reverseString(myString);
+        System.out.println("Reversed String: " + reversedString);
+    }
+
+    public static String reverseString(String str) {
+        StringBuilder reversed = new StringBuilder(str).reverse();
+        return reversed.toString();
+    }
 }
+

c30b71f9edae62dab26d14aea6097910ab98dade
fahadhasan <hasan.fahad67@outlook.com>
2023-11-01 03:05:46 +0600

    Sum Of Digit

---
+++ SumofDigit/SumofDigit.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class SumofDigit
+{
+	public static void main (String[]args)
+	{
+		Scanner input = new Scanner(System.in);
+		int num , temp , r ;
+		int sum = 0;
+        System.out.print("Enter any Number :");
+		num = input.nextInt();
+		
+		temp  = num;
+		while (temp != 0)
+		{
+			r = temp % 10;
+			sum = sum + r;
+			temp = temp / 10;
+		}
+		System.out.println("Sum of Digit "+sum);
+	}
+}

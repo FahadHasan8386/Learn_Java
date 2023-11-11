e1ad73ac029013a6bf60835564a91d390968c76d
fahadhasan <hasan.fahad67@outlook.com>
2023-11-12 01:43:47 +0600

    Sum of Object

--- MealSystem/MealSystem.java
+++ MealSystem/MealSystem.java
@@ -1,33 +1,52 @@
-class person{
-	String name;
-	double depositMoney;
-	
-	public person (String name , double depositMoney )
-	{
-		this.name = name;
-		this.depositMoney = depositMoney;
-	}
-	
-	public void display()
-	{
-		System.out.println("Name : "+name);
-		System.out.println("Total Deposit Money : "+depositMoney);			
-	}
-	
-	public void show(String s){
+// Base Class
+class person {
+    String name;
+    double depositMoney;
+    int millCount;
+
+    public person(String name, double depositMoney, int millCount) {
+        this.name = name;
+        this.depositMoney = depositMoney;
+        this.millCount = millCount;
+    }
+
+    public void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Total Deposit Money: " + depositMoney);
+        System.out.println("Total mill count: " + millCount);
+    }
+
+    public void show(String s) {
         System.out.println(s + " Details");
     }
+
+    public double getDepositMoney() {
+        return depositMoney;
+    }
+
+    public int getMillCount() {
+        return millCount;
+    }
 }
-public class MealSystem{
-	public static void main (String []args){
-		person p1 = new person ("Fahad" , 2000 );			
-		person p2 = new person ("Atik" , 500);
-		person p3 = new person ("Niloy" ,1000);
-		p1.show("Person 1");
-		p1.display();
-		p2.show("Person 2");
-		p2.display();
-		p3.show("Person 3");
-		p3.display();	
-	}
+
+public class MealSystem {
+    public static void main(String[] args) {
+        person p1 = new person("Fahad", 2000, 39);
+        person p2 = new person("Atik", 500, 10);
+        person p3 = new person("Niloy", 1000, 39);
+
+        p1.show("Person 1");
+        p1.display();
+        p2.show("Person 2");
+        p2.display();
+        p3.show("Person 3");
+        p3.display();
+
+        // Calculate and display the sum
+        double totalDepositMoney = p1.getDepositMoney() + p2.getDepositMoney() + p3.getDepositMoney();
+        int totalMillCount = p1.getMillCount() + p2.getMillCount() + p3.getMillCount();
+
+        System.out.println("\nTotal Deposit Money for all persons: " + totalDepositMoney);
+        System.out.println("Total mill count for all persons: " + totalMillCount);
+    }
 }

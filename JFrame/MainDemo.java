818f041fbb1d58f96357fbd87c500bffcca4955a
fahadhasan <hasan.fahad67@outlook.com>
2023-11-27 02:33:13 +0600

    JFrame

--- JFrame/MainDemo.java
+++ JFrame/MainDemo.java
@@ -6,6 +6,7 @@ public class MainDemo extends JFrame{
 		 private Container c;
 		 private JLabel L1 ,L2 ,L3 ,L4 ;
 		 private JTextField Tf1 ,Tf2 ,Tf3 ,Tf4;
+		 private JButton B1 ,B2, B3 ;
 		 
 	MainDemo()
 	{
@@ -25,12 +26,21 @@ public class MainDemo extends JFrame{
 		c.add(Tf1);
 		
 		L2= new JLabel ("Last Name ");
-		L2.setBounds(40 ,50 ,200 ,100 );
+		L2.setBounds(40 ,50 ,200 ,50 );
 		c.add(L2);
 		
 		Tf2 = new JTextField ();
-		Tf2.setBounds(120,40 ,100 ,100);
+		Tf2.setBounds(120,70 ,100 ,20);
 		c.add(Tf2);
+		
+		B1 = new JButton ("Save");
+		B1.setBounds(50 ,100 , 100 ,20 );
+		c.add(B1);
+		
+		B2 = new JButton ("Close");
+		B2.setBounds(150 ,100 , 100 ,20 );
+		c.add(B2);
+		
 		
 	}
 	

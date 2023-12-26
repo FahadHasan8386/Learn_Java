3caad0c29121cb586e64165fa728ad8f000f8c16
fahadhasan <hasan.fahad67@outlook.com>
2023-12-26 23:21:10 +0600

    String Array

---
+++ Demo/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String[] name = new String[3];
+        name[0] = "Fahad";
+        name[1] = "Hasan";
+        name[2] = "Niloy";
+
+        System.out.println(name.length);
+        for (int i = 0 ; i < name.length ; i++){
+            System.out.println(name[i]);
+        }
+
+    }
+}
